78feb46679f704926d1ace0ff68b7c7ada294857
gros-pataplouf <sceres@pm.me>
2024-09-22 22:11:32 +0200

    lesson 4 + 5

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue/dist/vue.js'
-console.log("hello from main")
 
 var app = new Vue({
   el:'#app',
@@ -8,12 +7,35 @@ var app = new Vue({
     description: "Dumbledore's favourite",
     image: "./assets/socksGreen.jpg",
     altText: "A pair of socks",
-    inventory: 4,
-    onSale: false
+    inventory: 0,
+    onSale: false,
+    inStock: false,
+    details: ["80% cotton", "20% polyester", "gender-neutral"],
+    variants: [
+      {id: 123456,
+      variantColor: "blue",
+      image: "./assets/socksBlue.jpg"
+      },
+      {
+        id: 654987,
+        variantColor: "green",
+        image: "./assets/socksGreen.jpg",
 
+      }
+    ],
+    sizes: ["S", "M", "L"],
+    cart: 0
+
+  },
+  methods: {
+    updateQuantity(num){
+      console.log(num)
+      this.cart += num;
+      this.inventory -= num
+    },
+    updateImage(asset){
+      this.image = asset;
+
+    }
   }
-
-
 })
-
-console.log("hello from main", app)
